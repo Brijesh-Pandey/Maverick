c429409a1970a0ada17d841f060f759d390dacd6
Brijesh Pandey <brijeshpandey18@gmail.com>
2023-04-28 08:11:05 +0530

    Added token

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import { useState } from "react";
 export default function App() {
   const [file, setFile] = useState(null);
 
+  const token = "c79a3b12-d111-41e5-acd4-756ee0d8b3bd:AT8ovDGPScstaDoySAm7AMaZSpc";
+
   const handleChange = (e) => {
     setFile(e.target.files[0]);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const url = "https://api.gragedev.in/wcdn/cdn/upload";
+    const url = "https://api.gragedev.in/cdn/wcdn/upload";
 
     try {
       const formData = new FormData();
       formData.append("files", file);
       formData.append("file_types", "img");
+
+      const header = {
+        Authorization: token,
+      }
       const response = await fetch(url, {
         method: "POST",
-        body: formData
+        body: formData,
+        headers: header
       });
       console.log(response);
     } catch (error) {
